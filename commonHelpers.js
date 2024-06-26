981179c43ac98a211a6b7f761446bd40d5b0b596
BohdanPrihodko <157399457+BohdanPrihodko@users.noreply.github.com>
2024-06-26 15:59:25 +0000

    Deploying to gh-pages from @ BohdanPrihodko/goit-js-hw-11@4f219acc8c25a21a23ff2c7ac938b9bd089d3b75 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
 import{S as d,i as n}from"./assets/vendor-0fc460d7.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const f="44474064-e47939977861edfa0fa12c2ac",m="https://pixabay.com/api/";async function p(a,t=1){const o=await fetch(`${m}?key=${f}&q=${a}&image_type=photo&orientation=horizontal&safesearch=true&page=${t}&per_page=12`);if(!o.ok)throw new Error("Failed to fetch images");return await o.json()}function y(a){const t=document.querySelector(".gallery"),o=a.map(({webformatURL:s,largeImageURL:e,tags:r,likes:i,views:c,comments:l,downloads:u})=>`
       <a class="gallery__item" href="${e}">
-        <img class="gallery__image" src="${s}" alt="${r}" />
+        <img class="gallery__image" src="${s}" width=360; height=200; alt="${r}" />
         <div class="info">
           <p>Likes: ${i}</p>
           <p>Views: ${c}</p>
